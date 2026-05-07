41de01c90801dfb8f499e19a11d8098011b3871e
Matya Aydin <119845483+MatyaAydin@users.noreply.github.com>
2026-05-07 22:09:25 +0200

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        shortest = float("inf")
+        finalBegin = finalEnd = 0
+        begin = end = 0
+        sizeS = len(s)
+        runningOcc = defaultdict(int)
+        occT = defaultdict(int)
+        for char in t:
+            occT[char] +=1
+        while end < sizeS:
+            runningOcc[s[end]] += 1
+            while begin <= end and isInside(runningOcc, occT):
+                shortest = min(shortest, end - begin +1)
+                if shortest == end - begin + 1:
+                    finalBegin = begin
+                    finalEnd = end
+                runningOcc[s[begin]] -= 1
+                if runningOcc[s[begin]] == 0:
+                    runningOcc.pop(s[begin])
+                begin +=1
+
+            end +=1
+        return s[finalBegin:finalEnd+1] if shortest != float("inf") else ""
+
+
+
+def isInside(occS, occT):
+    for char in occT.keys():
+        if occT[char] > occS.get(char, 0):
+            return False
+    return True
+        
