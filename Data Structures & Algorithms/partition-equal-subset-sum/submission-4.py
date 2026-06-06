98d55f01fb49b02ef2075d6629dc977245981f5b
Matya Aydin <119845483+MatyaAydin@users.noreply.github.com>
2026-06-06 18:46:29 +0200

    Add: partition-equal-subset-sum - submission-4

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums) % 2 != 0:
+            return False
+        n = len(nums)
+        found = False
+        memo = defaultdict(bool)
+        def backtrack(idx, sum1, sum2):
+            tup = (idx, sum1, sum2)
+            if idx == n:
+                memo[tup] = (sum1 == sum2)
+                return memo[tup]
+            
+            if tup in memo:
+                return memo[tup]
+            addLeft = backtrack(idx+1, sum1 + nums[idx], sum2)
+            addRight = backtrack(idx+1, sum1, sum2 + nums[idx])
+            memo[tup] = addLeft or addRight
+            return memo[tup]
+
+
+        backtrack(0, 0, 0)
+        return memo[(0, 0, 0)]  
+
+
+        
