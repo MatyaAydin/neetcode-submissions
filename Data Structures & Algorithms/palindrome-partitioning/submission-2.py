81f1abfe98d493363fb438fa1ac0dd46a41504ce
Matya Aydin <119845483+MatyaAydin@users.noreply.github.com>
2026-06-09 15:45:49 +0200

    Add: palindrome-partitioning - submission-2

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        allSubstrings = []
+        n = len(s)
+
+        def backtrack(currList, idx):
+            if idx == n:
+                allSubstrings.append(currList[:])
+                return
+            # backtrack(currList, idx + 1)
+            for j in range(idx, n):
+                if checkPalindrome(s, idx, j):
+                    currList.append(s[idx:j+1])
+                    backtrack(currList, j+1)
+                    currList.pop()
+                    # backtrack(currList, j+1)
+        backtrack([], 0)
+        return allSubstrings
+
+def checkPalindrome(s, left, right):
+    while left <= right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
+
+            
